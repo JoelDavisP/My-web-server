7adb810648092c0641fee0f5246a2b527e90015e
JoelDavisP <joeldavisp17195@gmail.com>
2017-12-08 16:28:18 +0530

    Add files via upload
    This is the final upload web server program

--- web_serv2.py
+++ web_serv2.py
@@ -1,11 +1,14 @@
+# 'This is a minimal web server project in python that receives request from client(s) (here browser) and send the requested resources back.'
+
+#' Usage: Run the code in python 2 editor and in browser run "http://localhost:5889/filename"'
+
+
 import os
 import socket
 import time
-
-
 def server_fun():
-	serv_add = (host, port) = ('', 5588)
-	max_clnt = 5
+	serv_add = (host, port) = ('', 5889)
+	max_clnt = 10
 	serv_soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	serv_soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 	serv_soc.bind(serv_add)
@@ -33,41 +36,34 @@ def process_req(clnt):
 			text_file = open(a,'rb').read()
 			response = 'HTTP/1.0 200 OK\n\n' + 'Content-type: text/html\n\n' + text_file + '\n'
 			clnt.sendall(response)
-			clnt.close()
-			time.sleep(30)
 		except:
 			err = ' Error 404 File not found\n\n' + 'Content-type: text/html\n\n'
 			clnt.sendall(err)
-			clnt.close()
-			time.sleep(30)
-    	if a.split('.')[1] == 'pdf':
+		clnt.close()
+		time.sleep(30)
+	if a.split('.')[1] == 'pdf':
 		try:
 			pdf_file = open(a,'rb').read()
 			response = 'HTTP/1.0 200 OK\n\n' + 'Content-type: text/html\n\n' + pdf_file + '\n'
 			clnt.sendall(response)
-			clnt.close()
-    			time.sleep(30)
 		except:
 			err = ' Error 404 File not found\n\n' + 'Content-type: text/html\n\n'
 			clnt.sendall(err)
-			clnt.close()
-			time.sleep(30)
+		clnt.close()
+		time.sleep(30)
 
 	if a.split('.')[1] == 'jpg' or a.split('.')[1] == 'jpeg':
-		path =  os.path.abspath(a)
-		size = os.path.getsize(path)
-		with open(a, 'rb') as img_file:
-			clnt.sendall('HTTP/1.0 200 OK\r\n' + 'Content-type: image/jpeg"\r\n' + img_file.read())
+		try:
+			size = os.path.getsize(path)
+			path =  os.path.abspath(a)
+			img_file = open(a, 'rb')
+			clnt.sendall('HTTP/1.1 200 OK\r\n' + 'Accept-Ranges: bytes\r\n' + 'Content-type: image/jpeg\r\n' + 'Content-Length: ' +str(size) +'\r\n\r\n')
+			clnt.sendall(img_file.read())
+		except:
+			err = ' Error 404 File not found\n\n' + 'Content-type: text/html\n\n'
+			clnt.sendall(err)
 		clnt.close()
-    		time.sleep(30)
-			 
-
-
-			#except:
-			#	im_cl = clnt.makefile('wb' , 0)
-			#	im_cl.write(' Error 404 File not found\n\n') 
-			#	im_cl.write('Content-type: text/html\n\n')
+		time.sleep(30)
 	
 if __name__ == '__main__':
     server_fun()
-
